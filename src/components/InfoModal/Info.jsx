b2de3f6c837cd28e9edfbacccbe346c365e9a867
Michael kane <wjsdpcks34@gmail.com>
2023-09-23 07:12:03 +0000

    Implement dynamic button style and grid layout

--- src/components/InfoModal/Info.jsx
+++ src/components/InfoModal/Info.jsx
@@ -6,8 +6,8 @@ import {useState} from "react";
 import styles from './Info.module.scss';
 
 const Info = ({isOpen, toggle}) => {
-	const [isMajor, setIsMajor] = useState(false);
-	const [isEverUsed, setIsEverUsed] = useState(false);
+	const [isMajor, setIsMajor] = useState(0);
+	const [isEverUsed, setIsEverUsed] = useState(0);
 	
 	const handleClick = (func, input) => {
 		func(input);
@@ -23,28 +23,67 @@ const Info = ({isOpen, toggle}) => {
 				<Modal.Body>
 					<Label>1. SW 관련 학과를 전공하셨나요? *</Label>
 					<Form.Group>
-						<Button color="basic" onClick={() => handleClick(setIsMajor, true)}>전공</Button>
-						<Button color="basic" onClick={() => handleClick(setIsMajor, false)}>비전공</Button>
+						<div className={cn(styles.FlexContainer)}>
+							<Button 
+								block 
+								onClick={() => handleClick(setIsMajor, 1)}
+								color={isMajor == 0 ? 'basic' : (isMajor == 1) ? 'primary' : 'basic'} 
+								outline={isMajor == 0 ? false : (isMajor == 1) ? true : false } 
+							>
+								전공
+							</Button>
+							<Button 
+								block 
+								onClick={() => handleClick(setIsMajor, 2)}
+								color={isMajor == 0 ? 'basic' : (isMajor == 1) ? 'basic' : 'primary'} 
+								outline={isMajor == 0 ? false : (isMajor == 1) ? false : true } 
+							>
+								비전공
+							</Button>
+						</div>
 					</Form.Group>
 					<Label>2. 구름 서비스를 사용해보신 적이 있나요? *</Label>
 					<Form.Group>
-						<Button color="basic" onClick={() => handleClick(setIsEverUsed, true)}>예</Button>
-						<Button color="basic" onClick={() => handleClick(setIsEverUsed, false)}>아니오</Button>
+						<div className={cn(styles.FlexContainer)}>
+							<Button 
+								block 
+								color={isEverUsed == 0 ? 'basic' : (isEverUsed == 1) ? 'primary' : 'basic'} 
+								outline={isEverUsed == 0 ? false : (isEverUsed == 1) ? true : false } 
+								onClick={() => handleClick(setIsEverUsed, 1)}
+							>
+								예
+							</Button>
+							<Button 
+								block 
+								color={isEverUsed == 0 ? 'basic' : (isEverUsed == 1) ? 'basic' : 'primary'}
+								outline={isEverUsed == 0 ? false : (isEverUsed == 1) ? false : true } 
+								onClick={() => handleClick(setIsEverUsed, 2)}
+							>
+								아니오
+							</Button>
+						</div>
 					</Form.Group>
 					
 					{
 						isEverUsed &&
 						<>
 							<Typography>2-1. 사용 경험이 있는 서비스를 선택해주세요.(복수 선택 가능)</Typography>
-							<div>
-								<Input type="checkbox" label="구름EDU"></Input>
-								<Input type="checkbox" label="구름LEVEL"></Input>
-								<Input type="checkbox" label="구름DEVTH"></Input>								
-							</div>
-
-							<div>
-								<Input type="checkbox" label="구름IDE"></Input>
-								<Input type="checkbox" label="구름EXP"></Input>
+							<div className={cn(styles.GridContainer)}>
+							  <div className={cn(styles.item)}>
+								<Input block type="checkbox" label="구름EDU" />
+							  </div>
+							  <div className={cn(styles.item)}>
+								<Input block type="checkbox" label="구름LEVEL" />
+							  </div>
+							  <div className={cn(styles.item)}>
+								<Input block type="checkbox" label="구름DEVTH" />
+							  </div>
+							  <div className={cn(styles.item)}>
+								<Input block type="checkbox" label="구름IDE" />
+							  </div>
+							  <div className={cn(styles.item)}>
+								<Input block type="checkbox" label="구름EXP" />
+							  </div>
 							</div>
 
 							<div>
