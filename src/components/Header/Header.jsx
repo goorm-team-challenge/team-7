b4e7b39eb6640f6119fdbb451fc78fac08837216
bybang <joshuabang26@gmail.com>
2023-09-23 06:35:43 +0000

    survey modal design finish

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,30 @@
+import { useState } from 'react';
 import cn from 'classnames';
+
+import { Survey } from '@/components';
 
 import { Button, Typography } from '@goorm-dev/gds-challenge';
 
 import styles from './Header.module.scss';
 
 const Header = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const handleOpen = () => {
+		setIsOpen((prev) => {
+			return !prev;
+		});
+	};
+
 	return (
 		<header className={cn(styles.header)}>
 			<div className={cn(styles.contents)}>
 				<Typography token="h5">
 					구름톤 챌린지 참여자 정보 수집
 				</Typography>
-				<Button size="lg">설문조사 참여하기</Button>
+				<Button size="lg" onClick={handleOpen}>
+					{isOpen && <Survey isOpen={isOpen} toggle={handleOpen} />}
+				</Button>
 			</div>
 		</header>
 	);
