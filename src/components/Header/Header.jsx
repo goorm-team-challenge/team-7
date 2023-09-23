edf3dcef6fb6862cd61454d59d817d7e75d25e2d
bybang <joshuabang26@gmail.com>
2023-09-23 07:52:28 +0000

    implement next page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,42 @@
 import { useState } from 'react';
 import cn from 'classnames';
 
-import { FeedBack, Survey } from '@/components';
+import { FeedBack, Info, ModalSign, Survey } from '@/components';
 
 import { Button, Typography } from '@goorm-dev/gds-challenge';
-import { Info } from '@/components';
-import { useState } from "react";
 
 import styles from './Header.module.scss';
 
 const Header = () => {
-	const [isOpen, setIsOpen] = useState(false);
-	
+	const [isOpen, setIsOpen] = useState(0);
+	const [toggle, setToggle] = useState(false);
+
 	const handleOpen = () => {
 		setIsOpen((prev) => {
-			return !prev;
+			return prev + 1;
 		});
 	};
-	
+
+	const handleToggle = () => {
+		setToggle(true);
+		setIsOpen(0);
+	};
+
 	return (
 		<header className={cn(styles.header)}>
 			<div className={cn(styles.contents)}>
 				<Typography token="h5">
 					구름톤 챌린지 참여자 정보 수집
 				</Typography>
-				<Button size="lg" onClick={handleOpen}>설문조사 참여하기</Button>
-				{isOpen && <Info isOpen={isOpen} toggle={handleOpen}/>}
+				<Button size="lg" onClick={handleOpen}>
+					설문조사 참여하기
+				</Button>
+				{!toggle && isOpen === 1 && (
+					<ModalSign isOpen={isOpen} increment={handleOpen} toggle={handleToggle} />
+				)}
+				{!toggle && isOpen === 2 && (
+					<Info isOpen={isOpen} toggle={handleToggle} />
+				)}
 				{/* {isOpen && <FeedBack isOpen={isOpen} toggle={handleOpen} />} */}
 			</div>
 		</header>
