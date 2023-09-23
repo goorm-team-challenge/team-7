5253335d2690502f1ef8ac5b05e949e3706d5299
최민우(풀스택 2회차) <cmw0918@naver.com>
2023-09-23 07:02:02 +0000

    main 페이지

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import { EmptyView, Header } from '@/components';
 
 import styles from './App.module.scss';
+import MainView from './components/MainView/MainView';
 
 function App() {
+	const users = [
+		{ id: 1, name: '최민우', email: 'email' },
+		{ id: 2, name: '�����우', email: 'email' },
+		{ id: 3, name: '최민우', email: 'email' },
+		{ id: 4, name: '�����우', email: 'email' },
+		{ id: 5, name: '최민우', email: 'email' },
+		{ id: 6, name: '�����우', email: 'email' },
+		{ id: 7, name: '최민우' },
+		{ id: 8, name: '�����우', email: 'email' },
+		{ id: 9, name: '최민우', email: 'email' },
+		{ id: 10, name: '�����우', email: 'email' },
+		{ id: 11, name: '최민우', email: 'email' },
+		{ id: 12, name: '�����우', email: 'email' },
+	];
 	return (
 		<div className={styles.App}>
 			<Header />
 			<main className={styles.main}>
-				<EmptyView />
+				{users ? <MainView props={users} /> : <EmptyView />}
 			</main>
 		</div>
 	);
